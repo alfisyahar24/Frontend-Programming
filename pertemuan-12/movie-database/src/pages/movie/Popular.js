31ea82ef03f979a50fdd90f93492f2be3146a216
alfisyahar <rena19013ti@student.nurulfikri.id>
2022-06-09 23:45:59 +0700

    menambahkan tugas pertemuan 12

---
+++ pertemuan-12/movie-database/src/pages/movie/Popular.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Hero from "../../components/Hero/Hero";
+import Movies from "../../components/Movies/Movies";
+import ENDPOINTS from "../../utils/constants/endpoints";
+
+function PopularMovie() {
+  // Simpan API KEY dan URL ke Variable
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  // const URL = `https://api.themoviedb.org/3/movie/popular/?api_key=${API_KEY}`;
+
+  // Membuat state movies
+  const [movies, setMovies] = useState([]);
+
+  /**
+   * Melakukan useEffect.
+   * useEffect to perform other jobs: fetch data
+   */
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+
+  async function getPopularMovies() {
+    /**
+     * Menggunakan library axios.
+     * Axios digunakan untuk melakukan fetch data.
+     */
+    const response = await axios(ENDPOINTS.POPULAR);
+
+    /**
+     * Simpan data movies dari axios ke state movies.
+     * Upadte state menggunakan setMovies().
+     */
+    setMovies(response.data.results);
+  }
+
+  /**
+   * Render Component Movies.
+   * Kirim props movies yang berisi nilai state movies
+   */
+  return (
+    <>
+      <Hero />
+      <Movies title="Popular Movies" movies={movies} />
+    </>
+  );
+}
+
+export default PopularMovie;
---
+++ pertemuan-12/movie-database/src/pages/movie/TopRated.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Hero from "../../components/Hero/Hero";
+import Movies from "../../components/Movies/Movies";
+import ENDPOINTS from "../../utils/constants/endpoints";
+
+function TopRatedMovie() {
+  // Simpan API KEY dan URL ke Variable
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  // const URL = `https://api.themoviedb.org/3/movie/top_rated/?api_key=${API_KEY}`;
+
+  // Membuat state movies
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getTopRatedMovies();
+  }, []);
+
+  async function getTopRatedMovies() {
+    /**
+     * Menggunakan library axios.
+     * Axios digunakan untuk melakukan fetch data.
+     */
+    const response = await axios(ENDPOINTS.TOPRATED);
+
+    /**
+     * Simpan data movies dari axios ke state movies.
+     * Upadte state menggunakan setMovies().
+     */
+    setMovies(response.data.results);
+  }
+
+  return (
+    <>
+      <Hero />
+      <Movies title="Top Rated Movies" movies={movies} />
+    </>
+  );
+}
+
+export default TopRatedMovie;
