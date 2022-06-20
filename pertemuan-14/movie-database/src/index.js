ac4995c9f55ec1fa0ef279cc219f4227d2f51717
alfisyahar <rena19013ti@student.nurulfikri.id>
2022-06-20 23:56:38 +0700

    menambahkan tugas pertemuan 14

--- pertemuan-14/movie-database/src/index.js
+++ pertemuan-14/movie-database/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import Browser Router dari React Router
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -14,9 +14,9 @@ ReactDOM.render(
      * Agar URL browser dapat dimanipulasi oleh React Router
      */}
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
