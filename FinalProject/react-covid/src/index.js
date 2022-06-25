2ae7275caf303022426cfb3378840ba1f3df5c5b
alfisyahar <rena19013ti@student.nurulfikri.id>
2022-06-25 23:39:19 +0700

    menambahkan final project

--- FinalProject/react-covid/src/index.js
+++ FinalProject/react-covid/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
